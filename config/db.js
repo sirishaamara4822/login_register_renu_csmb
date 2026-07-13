0617ad0469634c646c83731869a9f5128ae5e94b
sirishaamara4822 <sirishaamara4822@gmail.com>
2026-07-13 15:13:36 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
-
+const mongoose=required('mongoose');
+const dns = required("dns")
+dns.setServer(["8.8.8","8.8.4.4"])
 const connectDB = async () => {
   try {
     
-    await mongoose.connect('mongodb://localhost:27017/workshop_db');
+    await mongoose.connect('mongodb+srv://sirishaamara24:sirisha6281088042@cluster0.tzhsey4.mongodb.net/');
     
     console.log('MongoDB connected successfully!');
   } catch (err) {
